15c655fa1f9a6b9334f81ade5a5895d48abbeca2
name <isaacodey01@gmail.com>
2023-02-21 10:33:43 -0800

    print print putcharwq

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,20 @@
-#include <stdio.h>
 #include <main.h>
-
 /**
- * main - program that prints _putchar.
- *
- * Return: Always 0 (Success)
- */
-
-int main ()
+* main-print -purchar and add a new line
+*i
+* Return: always 0 (success)
+*/
+int main(void)
 {
-	printf ("_putchar");
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count = 0, size;
 
+	size = sizeof(str) / sizeof(int);
+	do {
+		_putchar(str[count]);
+		count++;
+	} while (count < size);
+	_putchar('\n');
 	return (0);
 }
+
