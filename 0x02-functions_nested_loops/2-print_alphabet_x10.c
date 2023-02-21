97e1b542ee3928fc0c5818b232cbca0b19dadf89
name <isaacodey01@gmail.com>
2023-02-21 12:37:47 -0800

    function to print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  *print_alphabet_x10 - Function that prints 10 times the alphabet, in lowercase.
+  *
+  *return (0) success
+  */
+
+void print_alphabet_x10(void)
+{
+	int i = 0, a;
+
+	while (i <= 10; i++)
+	{
+	
+		while (a = 97; a <= 122; a++)
+	{
+
+		putchar (a);
+	
+	}
+	
+		putchar ('\n');
+	}
+}
