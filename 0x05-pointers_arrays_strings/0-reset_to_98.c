4a84b619e6f832c5b8b16eb213be2acb4afa0f73
name <isaacodey01@gmail.com>
2023-02-27 16:04:43 -0800

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
-/**reset_to_90 - change value of n to 98
-  *
-  *Return: nothing.
-  */
+/** reset_to_99 - change value of n to 98
+ *
+ *Return: nothing.
+ */
 
 void reset_to_98(int *n)
 {
 	int *p;
+	int n;
+
 	p = &n;
 	*p = 98;
 }
