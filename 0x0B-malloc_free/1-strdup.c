b087921e1f1eb159abdecf9738278ea35afa977c
name <isaacodey01@gmail.com>
2023-03-15 07:44:34 -0700

    function that returns a pointer to a newly allocated space in memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,35 +1,31 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-  *str_concat - Concatenates two strings.
+  *_strdup - Returns a pointer to a newly-allocated space in memory
+  *containing a copy of the string given as parameter.
+  *@str: The string to be copied.
   *
-  *@s1: The string to be concatenated upon.
-  *@s2: The string to be concatenated to s1.
-  *
-  *Return: If concatenation fails - NULL.
+  *Return: If str == NULL or insufficient memory is available - NULL.
  */
-char *str_concat(char *s1, char *s2)
+char *_strdup(char *str)
 {
-	char *concat_str;
-	int index, concat_index = 0, len = 0;
+	char *duplicate;
+	int index, len = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	for (index = 0; s1[index] || s2[index]; index++)
-		len++;
-
-	concat_str = malloc(sizeof(char) * len);
-
-	if (concat_str == NULL)
+	if (str == NULL)
 		return (NULL);
 
-	for (index = 0; s1[index]; index++)
-		concat_str[concat_index++] = s1[index];
+	for (index = 0; str[index]; index++)
+		len++;
 
-	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s2[index];
-	return (concat_str);
+	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		duplicate[index] = str[index];
+
+	duplicate[len] = '\0';
+	return (duplicate);
 }
